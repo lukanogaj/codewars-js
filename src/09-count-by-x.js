994fc02d923722caf0ca31df46ac1ad9db6ae402
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-06-27 09:58:20 +0100

    add codewars kata 9 10 11

--- src/09-count-by-x.js
+++ src/09-count-by-x.js
@@ -11,9 +11,6 @@ x = 2, n = 5  --> [2,4,6,8,10]
 
 */
 
-const arrFirst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arrSecond = [2, 4, 6, 8, 10];
-
 function countBy(x, n) {
 	let z = [];
 	for (let i = 1; i <= n; i++) {
