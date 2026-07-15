aa6dfbde3ebbce937a6c16a610d44e88643bb4ad
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-07-15 09:05:58 +0100

    Add CodeWars kata 21 and update exercises

--- src/20-string-repeat.js
+++ src/20-string-repeat.js
@@ -18,4 +18,4 @@ function repeatStr(n, s) {
 	return s.repeat(n);
 }
 
-console.log(repeatStr(3, "Hello"));
+// console.log(repeatStr(3, "Hello"));
---
+++ src/21-abbreviate-a-two-word-name.js
@@ -0,0 +1,35 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+
+///////////
+Mehod
+split
+toUpperCase
+join
+
+*/
+
+const str12 = "Lukasz Nogaj";
+const words = str12.split();
+console.log(words);
+function abbrevName(name) {
+	// code away
+	const splitted = name
+		.split(" ")
+		.map((word) => word[0])
+		.join(".")
+		.toUpperCase();
+
+	return splitted;
+}
+
+// console.log(abbrevName(str12));
