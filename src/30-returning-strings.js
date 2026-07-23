32ed383f15704f6a7ed014abf9cce3f3c492207e
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-07-23 09:03:24 +0100

    Complete CodeWars 28-31

---
+++ src/30-returning-strings.js
@@ -0,0 +1,12 @@
+/*
+Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+*/
+function greet(name) {
+	//your code here
+	return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Lukasz"));
